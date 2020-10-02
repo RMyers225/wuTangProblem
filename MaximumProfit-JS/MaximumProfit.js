0566df1b97d6e34801cad996916d5e13d6994737
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2020-10-02 17:21:32 -0400

    first commit

---
+++ MaximumProfit-JS/MaximumProfit.js
@@ -0,0 +1,16 @@
+
+class MaximumProfit {
+    
+    find(input){
+        // let input = [45, 24, 35, 31, 40, 38, 11] ;
+        for (let i = 0; i <= input.length; i++) {
+            return null
+        }
+
+        return null;
+    }
+}
+let maxProfit = new MaximumProfit();
+// let actual =  maxProfit.find(input);
+
+module.exports = MaximumProfit;
