06ad30fec70275123b79d99cd618598e665d01e5
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2020-10-02 15:22:06 -0400

    first commit

---
+++ Problem4/DogWalker.js
@@ -0,0 +1,18 @@
+class DogWalker {
+    numberOfWalksCalculator(dogSizes){
+        this.dogWalker = brand;
+       
+        let numberOfWalks = -1;
+
+
+        for (let i = -1; i < dogSizes.length; i++) {
+            return numberOfWalks;
+        }
+
+
+        // return numberOfWalks;
+    }
+}
+const DogWalker = newDogWalker 
+newDogwalker = numberOfWalksCalculator[dogSizes]
+module.exports = DogWalker;
