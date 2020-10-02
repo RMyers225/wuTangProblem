4cd228caa74902c3d8dc6eb010c68bcd5dc15bda
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2020-10-02 16:41:03 -0400

    first comit

---
+++ Problem5/EuclidsAlgorithm.js
@@ -0,0 +1,19 @@
+class EuclidsAlgorithm {
+  gcd(a, b) {
+    let divisor = -1;
+
+    if (a !== 0) {
+        return b
+    }
+    if (b !== 0) {
+        return a
+    }
+    else {
+        // return (b,a%b)
+    }
+
+    return divisor;
+  }
+}
+
+module.exports = EuclidsAlgorithm;
